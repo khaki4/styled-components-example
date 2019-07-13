82ddb1bbcc7ca30e5ece1dba6bc942f785b8d16e
pi3rrot <4dakto@gmail.com>
2019-07-13 13:17:30 +0900

    mixins

--- README.md
+++ README.md
@@ -132,3 +132,22 @@ function App() {
   );
 }
 ```
+
+## Mixins : css group
+```javascript
+
+const awesomeCard = css`
+  border: none;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 1000%;
+  background-color: #bdc3c7;
+  ${awesomeCard}
+`;
+```
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { createGlobalStyle, keyframes } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -7,10 +7,24 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const awesomeCard = css`
+  border: none;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 1000%;
+  background-color: #bdc3c7;
+`;
+
 const Input = styled.input.attrs({
   required: true
 })`
-  border-radius:5px;
+  ${awesomeCard}
 `;
 
 function App() {
@@ -23,12 +37,5 @@ function App() {
     </>
   );
 }
-
-const Container = styled.div`
-  height: 100vh;
-  width: 1000%;
-  background-color: #bdc3c7;
-`;
-
 
 export default App;
